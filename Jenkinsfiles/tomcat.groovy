a85ad718ee4af49305157a9e50e04fd837131f2a
Amogh Jagadale <a.jagadale@wirecard.com>
2020-02-07 01:44:08 +0100

    updating pipeline to reference shared library

--- Jenkinsfiles/tomcat.groovy
+++ Jenkinsfiles/tomcat.groovy
@@ -1,33 +1,22 @@
-private void serviceCheck() {
-    RETURN_STATUS   = sh(returnStatus: true, script: 'sudo service tomcat status')
-    if ("${params.ACTION}" == 'start' || "${params.ACTION}" == 'restart') {
-        if (RETURN_STATUS != 0) {
-            currentBuild.result = 'FAILURE'
-        }
-    } else if ("${params.ACTION}" == 'stop') {
-        if (RETURN_STATUS != 0) {
-            println ("Script exit code: ${RETURN_STATUS}")
-            currentBuild.result = 'SUCCESS'
-        }
-    }
-}
+@Library('jenkins-shared-lib')_
 
 pipeline {
     agent none
     parameters {
         string(name: 'LABEL', defaultValue: 'qa&&web&&helloworld', description: 'Node label')
-        choice(name: 'ACTION', choices: ['', 'start', 'stop', 'restart'], description: 'Action')
+        choice(name: 'ACTION', choices: ['start', 'stop', 'restart'], description: 'Action')
     }
     stages {
-        stage('Tomcat Operation') {
+        stage('Nginx Operation') {
             agent { label "${params.LABEL}" }
             steps {
-                sh "sudo service tomcat ${params.ACTION}"    
+                service name: "nginx" ,action: "${params.ACTION}"
+
             }
             post {
-                success {
+                always {
                     script {
-                        serviceCheck()
+                        service.buildStatus("nginx", "${params.ACTION}")
                     }
                 }
             } 
