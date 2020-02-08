2fe7ffdc20a06fa8c0da29d92015afb07311b588
Amogh Jagadale <a.jagadale@wirecard.com>
2020-02-08 22:25:42 +0100

    removing root privillages

--- src/org/jenkins/helper.groovy
+++ src/org/jenkins/helper.groovy
@@ -1,8 +1,8 @@
 package org.jenkins
 
 public void deploy() {
-    sh "sudo rm -rf /opt/tomcat/webapps/ROOT/*"
-    sh "sudo unzip -o *.zip"
-    sh "sudo find target/ -name '*.war' -exec mv {} /opt/tomcat/webapps/ROOT/ \\;"
-    sh "sudo unzip -o /opt/tomcat/webapps/ROOT/*.war"
+    sh "rm -rf /opt/tomcat/webapps/ROOT/*"
+    sh "unzip -o *.zip"
+    sh "find target/ -name '*.war' -exec mv {} /opt/tomcat/webapps/ROOT/ \\;"
+    sh "unzip -o /opt/tomcat/webapps/ROOT/*.war"
 }
