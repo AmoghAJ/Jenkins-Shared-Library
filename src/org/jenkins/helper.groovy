f4d6620493dad1d8090a08c23a73146f96f79211
Amogh Jagadale <a.jagadale@wirecard.com>
2020-02-08 22:32:02 +0100

    redirecting extraction to right directory

--- src/org/jenkins/helper.groovy
+++ src/org/jenkins/helper.groovy
@@ -4,5 +4,5 @@ public void deploy() {
     sh "rm -rf /opt/tomcat/webapps/ROOT/*"
     sh "unzip -o *.zip"
     sh "find target/ -name '*.war' -exec mv {} /opt/tomcat/webapps/ROOT/ \\;"
-    sh "unzip -o /opt/tomcat/webapps/ROOT/*.war"
+    sh "unzip -o /opt/tomcat/webapps/ROOT/*.war -d /opt/tomcat/webapps/ROOT/"
 }
