91b61e18b769c4afcef5b54c0b7bd41cf66d96a6
Amogh Jagadale <a.jagadale@wirecard.com>
2020-02-08 22:05:03 +0100

    escape character handling

--- src/org/jenkins/helper.groovy
+++ src/org/jenkins/helper.groovy
@@ -3,6 +3,6 @@ package org.jenkins
 public void deploy() {
     sh "sudo rm -rf /opt/tomcat/webapps/ROOT/*"
     sh "sudo unzip *.zip"
-    sh "sudo find target/ -name '*.war' -exec mv {} /opt/tomcat/webapps/ROOT/ \;"
+    sh "sudo find target/ -name '*.war' -exec mv {} /opt/tomcat/webapps/ROOT/ \\;"
     sh "sudo unzip /opt/tomcat/webapps/ROOT/*.war"
 }
