d91166b554245872eec1ac923f6195c221379586
Amogh Jagadale <a.jagadale@wirecard.com>
2020-02-09 23:16:53 +0100

    trying JsonSlurperClassic

--- src/org/jenkins/helper.groovy
+++ src/org/jenkins/helper.groovy
@@ -1,6 +1,7 @@
 package org.jenkins
 
-import groovy.json.JsonSlurper
+// import groovy.json.JsonSlurper
+import groovy.json.JsonSlurperClassic
 
 public void deploy() {
     sh "rm -rf /opt/tomcat/webapps/ROOT/*"
@@ -15,7 +16,9 @@ private String resources() {
 }
 
 public def resources_map() {
-    def jsonSlurper = new JsonSlurper()
-    String resource_string_out = resources()   
-    return new HashMap<>(jsonSlurper.parseText(resource_string_out))
+    // def jsonSlurper = new JsonSlurper()
+    def JsonSlurperClassic = new groovy.json.JsonSlurperClassic()
+    String resource_string_out = resources()
+    return new HashMap<> (JsonSlurperClassic.parseText(resource_string_out))    
+    // return new HashMap<>(jsonSlurper.parseText(resource_string_out))
 }
