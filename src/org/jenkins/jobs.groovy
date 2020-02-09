85b372ba9b55d3875a9ad788391f385be8189968
Amogh Jagadale <a.jagadale@wirecard.com>
2020-02-09 22:47:03 +0100

    adding missing commas

--- src/org/jenkins/jobs.groovy
+++ src/org/jenkins/jobs.groovy
@@ -3,7 +3,7 @@ package org.jenkins
 public void haproxy(String lb_node, String web_node, String action) {
     build job: 'haproxy_operations', 
           parameters: [string(name: 'HAPROXY_NODE', value: lb_node),
-                       string(name: 'APP_NODE', value: web_node)
+                       string(name: 'APP_NODE', value: web_node),
                        string(name: 'ACTION', value: action)]
 }
 
@@ -16,7 +16,7 @@ public void nginx(String web_node, String action) {
 public void haproxy(String s3_path, String app_node, String version, String env) {
     build job: 'tomcat_deploy', 
           parameters: [string(name: 'SOFTWARE_S3_PATH', value: s3_path),
-                       string(name: 'APP_NODE', value: app_node)
+                       string(name: 'APP_NODE', value: app_node),
                        string(name: 'VERSION', value: version),
                        string(name: 'ENVIRONMENT', value: env)]
 }
