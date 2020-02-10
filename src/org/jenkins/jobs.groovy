5e4e4d26cad0a2f05c0598c9d836cad4be31f08d
Amogh Jagadale <a.jagadale@wirecard.com>
2020-02-10 15:40:45 +0100

    adding wait for job triggers

--- src/org/jenkins/jobs.groovy
+++ src/org/jenkins/jobs.groovy
@@ -4,13 +4,17 @@ public void haproxy(String lb_node, String web_node, String action) {
     build job: 'haproxy_operations', 
           parameters: [string(name: 'HAPROXY_NODE', value: lb_node),
                        string(name: 'APP_NODE', value: web_node),
-                       string(name: 'ACTION', value: action)]
+                       string(name: 'ACTION', value: action)],
+          wait: true,
+          propagate: true
 }
 
 public void nginx(String web_node, String action) {
     build job: 'nginx_operation', 
           parameters: [string(name: 'LABEL', value: web_node),
-                       string(name: 'ACTION', value: action)]
+                       string(name: 'ACTION', value: action)],
+          wait: true,
+          propagate: true
 }
 
 public void tomcat_deploy(String s3_path, String app_node, String version, String env) {
@@ -18,5 +22,7 @@ public void tomcat_deploy(String s3_path, String app_node, String version, Strin
           parameters: [string(name: 'SOFTWARE_S3_PATH', value: s3_path),
                        string(name: 'APP_NODE', value: app_node),
                        string(name: 'VERSION', value: version),
-                       string(name: 'ENVIRONMENT', value: env)]
+                       string(name: 'ENVIRONMENT', value: env)],
+          wait: true,
+          propagate: true
 }
