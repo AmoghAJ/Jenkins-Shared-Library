3d60a7bd9acd5ecc394dd286a0edf272b4eff576
Amogh Jagadale <a.jagadale@wirecard.com>
2020-02-11 00:18:18 +0100

    introducing releaseSequenceParallel

--- src/org/jenkins/releaseHelper.groovy
+++ src/org/jenkins/releaseHelper.groovy
@@ -5,7 +5,7 @@ evaluate(new File("jobs.groovy"))
 
 public def helper  = new helper()
 
-private def releaseSequence(String lb_node, String web_node, String app_node, String s3_path, String version, String env) {
+private void releaseSequence(String lb_node, String web_node, String app_node, String s3_path, String version, String env) {
     jobs    = new jobs()
     jobs.haproxy(lb_node, web_node, 'disable')
     jobs.nginx(web_node, 'stop')
@@ -13,6 +13,19 @@ private def releaseSequence(String lb_node, String web_node, String app_node, St
     jobs.nginx(web_node, 'start')
     helper.checkHttpResponse(web_node)
     jobs.haproxy(lb_node, web_node, 'enable')
+}
+
+private def releaseSequenceParallel(String lb_node, String web_node, String app_node, String s3_path, String version, String env) {
+    jobs    = new jobs()
+    def executionSeq = [
+        jobs.haproxy(lb_node, web_node, 'disable'),
+        jobs.nginx(web_node, 'stop'),
+        jobs.tomcat_deploy(s3_path, app_node, version, env),
+        jobs.nginx(web_node, 'start'),
+        helper.checkHttpResponse(web_node),
+        jobs.haproxy(lb_node, web_node, 'enable')
+    ]
+    return executionSeq
 }
 
 def release(String app, String s3_path, String version, String env) {
@@ -52,7 +65,7 @@ def releaseProd(String app, String s3_path, String version, String env, Boolean 
                                                                                 //  version,
             
                                                                                 //  env))
-            def jobs = [releaseSequence(data['apps'][app]['infra'][env]['lb'][0], 
+            def jobs = [releaseSequenceParallel(data['apps'][app]['infra'][env]['lb'][0], 
                                           data['apps'][app]['infra'][env]['web'][x], 
                                           data['apps'][app]['infra'][env]['app'][x],
                                           s3_path,
