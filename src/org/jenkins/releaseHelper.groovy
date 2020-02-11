83833f31f7780c74dc21fcb371b07f368296571e
Amogh Jagadale <a.jagadale@wirecard.com>
2020-02-11 17:35:44 +0100

    trying map generation using put

--- src/org/jenkins/releaseHelper.groovy
+++ src/org/jenkins/releaseHelper.groovy
@@ -58,23 +58,23 @@ def releaseProd(String app, String s3_path, String version, String env, Boolean 
     if (parallelDeployment && nodes >= 2) {
         jobHelper   = new jobs()
         jobs        = jobHelper.createJob(this)
-        def builds  = []
+        def builds  = [:]
         for(int x=0; x<=nodes; x++) {
 
             def lbNode      = data['apps'][app]['infra'][env]['lb'][0]
             def webNode = data['apps'][app]['infra'][env]['web'][x]
             def appNode = data['apps'][app]['infra'][env]['app'][x]
             
-            def releaseSeq = "Deploy ${appNode}": {
+            def releaseSeq =  [{
                                     [jobs.haproxy(lbNode, webNode, 'disable'),
                                     jobs.nginx(webNode, 'stop'),
                                     jobs.tomcat_deploy(s3_path, appNode, version, env),
                                     jobs.nginx(webNode, 'start'),
                                     helper.checkHttpResponse(webNode),
                                     jobs.haproxy(lbNode, webNode, 'enable')]
-                                }
+                                }]
             
-            builds.add(releaseSeq)
+            builds.put("Deploy ${appNode}", releaseSeq)
         }
         println(builds)
         parallel(builds) 
