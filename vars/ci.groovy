b58723b7fd5ae931896aa39ffd4dd242d98ca790
Amogh Jagadale <a.jagadale@wirecard.com>
2020-02-12 14:35:46 +0100

    moving ci from jenkinsfile

---
+++ vars/ci.groovy
@@ -0,0 +1,67 @@
+#!/usr/bin/groovy
+
+import org.jenkins.*
+
+def call(StageParameters) {
+    // def config = [:]
+    // body.resolveStrategy = Closure.DELEGATE_FIRST
+    // body.delegate = config
+
+    pipeline {
+        agent { label 'ci' }
+        options { 
+            timestamps()
+            buildDiscarder(logRotator(numToKeepStr: '5')) 
+            }
+        environment{
+            APPLCICATION   = 'hello-world'
+            ARTIFACT_ZIP   = misc.artifactZip(APPLCICATION)
+            S3_BUCKET      = misc.artifactBucket(APPLCICATION)
+        }
+        stages {
+            stage('Build') {
+                steps {
+                    mavenOperations action: "clean install -U"
+                }
+                post {
+                    always {
+                        junit 'target/surefire-reports/*.xml'
+                    }
+                    success {
+                        script {
+                            misc.packageArtifact(ARTIFACT_ZIP, "target/*.war")
+                            awss3cp s3_object: ARTIFACT_ZIP,destination: misc.s3BucketPadding(S3_BUCKET)   
+                            archiveArtifacts artifacts: 'target/*.war', fingerprint: true
+                        }
+                    }
+                    failure {
+                        println "Slack notification: Build stage failure"
+                    }
+                }
+            }
+            stage('Integration Test') {
+                when {
+                    branch 'master'
+                }
+                steps {
+                    println 'Integration testing'
+                }
+            }
+            stage('Schedule release') {
+                when{
+                    branch 'master'
+                }
+                steps{
+                    script {
+                        def config = StageParameters.config
+                        println "Version:" + config.version
+                        println config.deploy.qa
+                        println config.deploy.test
+                        println config.deploy.prod
+                        println "Type: $(config.deploy.qa).getClass()"
+                    }
+                }
+            }
+        }
+    }
+}
